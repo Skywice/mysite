430b361a9be9f6159d53aaa87c30cf7c1b8e9078
王乾 <1092521171@qq.com>
2016-10-02 22:40:54 +0800

    After tutorial 4 change

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls import url
-from . import views
+from . import views_old
 
 app_name = 'polls'
+'''
+精简之前的UrlConf
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),  # ^ $ 分别匹配字符串的开头和结尾
@@ -12,4 +14,13 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/votes/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
+'''
+# 修改之后
+urlpatterns = [
+    url(r'^$', views_old.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views_old.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views_old.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views_old.vote, name='vote'),
+
 ]
--- polls/views_old.py
+++ polls/views_old.py
@@ -2,10 +2,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -27,9 +28,24 @@ def detail(request, question_id):
     return render(request,'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = 'You\'re looking at the result of question %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse('You\'re voting on question %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])  # ?
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 反解析url以直接访问其它视图方法
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
 
