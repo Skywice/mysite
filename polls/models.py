7573f43b3ca61236de76f9a131a2e3dcc695fc1e
王乾 <1092521171@qq.com>
2016-10-07 23:04:48 +0800

    20161007 for more modeification

--- polls/models.py
+++ polls/models.py
@@ -24,7 +24,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)  # 是否发布一天以上
+        # 当前时间
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now  # 是否发布一天以上
 
 @python_2_unicode_compatible
 class Choice(models.Model):
--- polls/views_old.py
+++ polls/views_old.py
@@ -34,7 +34,7 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])  # ?
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])  # request里面包含POST与GET的信息
     except(KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html',{
             'question': question,
